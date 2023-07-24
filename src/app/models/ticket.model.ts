c6baaef06cd134abe75649db816596240670624d
Kukesan <kkukesan@gmail.com>
2023-07-24 23:09:54 +0530

    Auto-generated commit [ci skip]

--- src/app/models/ticket.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Ticket{
-    id: number;
-    category : number;
-    contactName :string;
-    contactNumber : string;
-    seatsCount : number;
-    dateTime : string;
-    description : string;
-}
